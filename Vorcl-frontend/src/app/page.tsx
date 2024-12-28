145b8aa055b42a784c4b306b46168159e3dd3710
1Rendes <1admin@ukr.net>
2024-12-28 13:57:56 +0100

    voice-visualizer add

--- Vorcl-frontend/src/app/page.tsx
+++ Vorcl-frontend/src/app/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-// import { useLottie } from 'lottie-react';
+import { Visualizer } from 'react-sound-visualizer';
 import blueMicro from '../../assets/blue-micro.json';
 import redMicro from '../../assets/red-micro.json';
 import { Player } from '@lottiefiles/react-lottie-player';
@@ -8,18 +8,54 @@ import { useState } from 'react';
 
 export default function Home() {
   const [recording, setRecording] = useState(false);
-  const handleClick = () => {
-    if (!recording) {
-      setRecording(true);
+  const [mediaStream, setMediaStream] = useState<MediaStream | null>(null);
+  const [mediaRecorder, setMediaRecorder] = useState<MediaRecorder | null>(
+    null,
+  );
+
+  const handleClick = async () => {
+    if (recording) {
+      if (mediaStream) {
+        mediaStream.getTracks().forEach((track) => track.stop());
+        setMediaStream(null);
+        setRecording(false);
+        console.log('Microphone recording stopped');
+      }
     } else {
-      setRecording(false);
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          audio: true,
+        });
+        setMediaStream(stream);
+        setRecording(true);
+        const recorder = new MediaRecorder(stream);
+        setMediaRecorder(recorder);
+      } catch (error) {
+        console.error('No permission to microphone, ', error);
+        setRecording(false);
+      }
     }
   };
   return (
-    <div className="bg-audio-background rounded-[24px] m-auto flex flex-col gap-[20px] w-[552px] h-[313px]">
-      <h3 className="text-center mt-[77px] text-white text-xs">
-        Start a conversation with assistants
-      </h3>
+    <div className="bg-audio-background rounded-[24px] m-auto flex flex-col pt-[20px] w-[552px] h-[313px]">
+      <div className=" flex justify-center items-center text-white text-xs h-[100]">
+        {mediaStream ? (
+          <Visualizer
+            audio={mediaStream}
+            autoStart={recording}
+            slices={4}
+            strokeColor={'#9013FE'}
+            rectWidth={30}
+            barRadius={20}
+          >
+            {({ canvasRef }) => (
+              <canvas ref={canvasRef} width={200} height={100} />
+            )}
+          </Visualizer>
+        ) : (
+          'Start a conversation with assistants'
+        )}
+      </div>
       <button
         onClick={handleClick}
         className={`flex justify-center align-middle `}
@@ -28,7 +64,7 @@ export default function Home() {
           autoplay={recording}
           loop
           src={recording ? redMicro : blueMicro}
-          style={{ height: '170px', width: '170px' }}
+          style={{ height: '180px', width: '180px' }}
         />
       </button>
     </div>
