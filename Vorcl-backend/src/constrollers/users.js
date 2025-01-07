c0b0e89a75fc839d8de75d5a5dadce6f79f8ac9a
1Rendes <1admin@ukr.net>
2025-01-07 14:53:31 +0100

    backend form done

--- Vorcl-backend/src/constrollers/users.js
+++ Vorcl-backend/src/constrollers/users.js
@@ -1,8 +1,16 @@
+import { registerUser } from "../services/mongo.js";
+
 export const usersController = async (req, reply) => {
   const { email } = req.body;
   if (!email) reply.status(201).send("email is required");
-
   try {
     const user = await registerUser(email);
-  } catch (error) {}
+    reply.status(201).send({
+      status: 201,
+      message: "user succesfully created.",
+      data: user,
+    });
+  } catch ({ status, message }) {
+    reply.status(status).send({ status, message });
+  }
 };
--- Vorcl-backend/src/services/mongo.js
+++ Vorcl-backend/src/services/mongo.js
@@ -1,1 +1,8 @@
-const registerUser = async () => {};
+import createHttpError from "http-errors";
+import { UserCollection } from "../db/models/user.js";
+
+export const registerUser = async (email) => {
+  const user = await UserCollection.findOne({ email });
+  if (user) throw createHttpError(409, "Email in use");
+  return UserCollection.create({ email });
+};
